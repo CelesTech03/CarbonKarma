e3f81b8303b95f9e9fb17516f6b3990436687eea
CelesTech03 <celeste.urena@stu.bmcc.cuny.edu>
2023-03-11 15:38:05 -0500

    Minor changes to login screen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,8 +1,12 @@
 import { KeyboardAvoidingView, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { useState } from "react";
 import React from "react";
 import styles from "./styles/AuthStyle"
 
 const LoginScreen = ({ navigation }) => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
     // KeyboardAvoidingView = Prevents keyboard from blocking input fields
     <KeyboardAvoidingView style={styles.container} behavior="padding">
@@ -11,10 +15,18 @@ const LoginScreen = ({ navigation }) => {
       {/* Inputs View */}
       <View style={styles.inputContainer}>
         <Text style={styles.labelText}>Email:</Text>
-        <TextInput placeholder="Email" style={styles.input} />
+        <TextInput 
+        placeholder="Email" 
+        value={email}
+        onChangeText={text => setEmail(text)}
+        style={styles.input} 
+        required
+        />
         <Text style={styles.labelText}>Password:</Text>
         <TextInput
           placeholder="Password"
+          value={password}
+          onChangeText={text => setPassword(text)}
           style={styles.input}
           // Obscures users' password
           secureTextEntry
@@ -24,7 +36,7 @@ const LoginScreen = ({ navigation }) => {
       {/* Buttons View */}
       <View style={styles.buttonContainer}>
         {/* TouchableOpacity = A wrapper for making views respond properly to touches */}
-        <TouchableOpacity onPress={() => navigation.navigate("Homepage")} 
+        <TouchableOpacity onPress={() => navigation.navigate("Homepage")} onPressIn={this.validates}
         style={styles.button}>
           <Text style={styles.buttonText}>Login</Text>
         </TouchableOpacity>
