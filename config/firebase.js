625ca7a2c22794e06be6b820b3d9c2ba3c8a93a5
CelesTech03 <celeste.urena@stu.bmcc.cuny.edu>
2023-03-18 19:05:44 -0400

    Added connection to Firebase Database

--- config/firebase.js
+++ config/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import firebase from 'firebase/compat/app'
 import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 import {API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID, MEASUREMENT_ID} from "@env"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -26,5 +27,33 @@ if (firebase.apps.length == 0) {
 }
 
 const auth = firebase.auth();
-
 export { auth };
+
+const firestore = firebase.firestore();
+
+// Creates new user document (data)
+export const createUserDocument = async (user, additionalData) => {
+  if (!user) return;
+
+  // Gets user reference with the path users/ (user colelction) and a unique user id
+  const userRef = firestore.doc(`users/${user.uid}`);
+  // Fetches document at this location
+  const snapshot = await userRef.get();
+
+  // If there is no document (snapshot) of user then create one
+  if (!snapshot.exists) {
+    const {email} = user;
+    const {userName, fullName} = additionalData;
+
+    try {
+      userRef.set({
+        email,
+        fullName,
+        userName,
+        createdAt: new Date(),
+      });
+    } catch(error) {
+      console.log('Error in creating user', error);
+    }
+  }
+}
