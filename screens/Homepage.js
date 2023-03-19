b8589db685e48a2ffce8bdbc28742d150cc90755
CelesTech03 <celeste.urena@stu.bmcc.cuny.edu>
2023-03-18 20:17:32 -0400

    Added fetch and display users data in homepage

--- screens/Homepage.js
+++ screens/Homepage.js
@@ -1,13 +1,55 @@
-import { KeyboardAvoidingView, Text, TextInput, TouchableOpacity, View } from "react-native";
-import { React } from 'react'
-import styles from "./styles/AuthStyle"
+import {
+  KeyboardAvoidingView,
+  Text,
+  View,
+} from "react-native";
+import { React, useState, useEffect } from "react";
+import styles from "./styles/AuthStyle";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
-const Homepage = ({ navigation }) =>  {
+const Homepage = () => {
+  const [user, setUser] = useState(null);
+  const db = firebase.firestore();
+
+  // Fetches user data from the users collection in database using their uid (unique id)
+  useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        db.collection("users")
+          .doc(user.uid)
+          .get()
+          .then((snapshot) => {
+            if (snapshot.exists) {
+              setUser(snapshot.data());
+            }
+          });
+      }
+    });
+    return () => unsubscribe();
+  }, []);
+
+  // Displays loading screen while data is being fetched
+  if (!user) {
+    return (
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
+        <View>
+          <Text>Loading...</Text>
+        </View>
+      </KeyboardAvoidingView>
+    );
+  }
+
   return (
     <KeyboardAvoidingView style={styles.container} behavior="padding">
-      <Text>Score: 342</Text>    
+      <View>
+        <Text>Welcome, {user.fullName}!</Text>
+        <Text>Your username is: {user.userName}</Text>
+        <Text>Score: 342</Text>  
+      </View>
     </KeyboardAvoidingView>
   );
 };
 
-export default Homepage
+export default Homepage;
